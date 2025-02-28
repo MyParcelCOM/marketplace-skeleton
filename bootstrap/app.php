2b459ace3057db32f4edb5efe007b6a608a8ac0a
Daniel Verner <daniel@myparcel.com>
2025-02-28 09:05:42 +0100

    :heavy_minus_sign: Removed unused imports

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,31 +1,10 @@
 <?php
+
 declare(strict_types=1);
 
-use App\Http\Middleware\Authenticate;
-use App\Http\Middleware\PreventRequestsDuringMaintenance;
-use App\Http\Middleware\TrimStrings;
-use App\Http\Middleware\TrustProxies;
-use Illuminate\Auth\Middleware\AuthenticateWithBasicAuth;
-use Illuminate\Auth\Middleware\Authorize;
-use Illuminate\Auth\Middleware\EnsureEmailIsVerified;
-use Illuminate\Auth\Middleware\RequirePassword;
 use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull;
-use Illuminate\Foundation\Http\Middleware\ValidatePostSize;
-use Illuminate\Http\Middleware\HandleCors;
-use Illuminate\Http\Middleware\SetCacheHeaders;
-use Illuminate\Routing\Middleware\SubstituteBindings;
-use Illuminate\Routing\Middleware\ThrottleRequests;
-use Illuminate\Routing\Middleware\ValidateSignature;
-use Illuminate\Validation\ValidationException;
 use MyParcelCom\ConcurrencySafeMigrations\Commands\Migrate;
 use MyParcelCom\Integration\Exceptions\ExceptionMapper;
-use MyParcelCom\Integration\Http\Middleware\MatchingChannelOnly;
-use MyParcelCom\Integration\Http\Middleware\MiddlewareMapper;
-use MyParcelCom\Integration\Http\Middleware\TransformsManyToJsonApi;
-use MyParcelCom\Integration\Http\Middleware\TransformsOneToJsonApi;
 
 /*
 |--------------------------------------------------------------------------
@@ -40,8 +19,7 @@ use MyParcelCom\Integration\Http\Middleware\TransformsOneToJsonApi;
 
 return Application::configure(basePath: $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__))
     ->withExceptions(new ExceptionMapper($_ENV['APP_DEBUG'] ?? false))
-    ->withMiddleware(new MiddlewareMapper())
     ->withCommands([
-        Migrate::class
+        Migrate::class,
     ])
     ->create();
